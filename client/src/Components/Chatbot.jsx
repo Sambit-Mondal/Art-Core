4cbbc5bcc2d4498230290bf13bc2e1cda9345a8a
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-04 20:17:26 +0530

    Update Chatbot.jsx

--- client/src/Components/Chatbot.jsx
+++ client/src/Components/Chatbot.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import chatbot from '../assets/Chatbot.png'
 
 function Chatbot() {
   return (
-    <div>
-        
+    <div className='select-none fixed flex items-center justify-center bottom-6 right-6 overflow-hidden w-16 h-16 cursor-pointer rounded-full z-50'>
+        <img src={chatbot} alt="Chatbot" />
     </div>
   )
 }
