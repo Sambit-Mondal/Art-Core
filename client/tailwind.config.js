afb31001d2c3367642138ba43cc7ea3a754b0a51
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-05 18:26:26 +0530

    Updated tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
       'navbar': '#C2C2C2',
       'black': '#000000',
       'white': '#FFFFFF',
-      'activeTab': '#6E6E6E'
+      'activeTab': '#6E6E6E',
+      'hoverTabs': '#949494'
+    },
+    fontFamily: {
+      'jim': ['"Jim Nightshade"'],
+      'carter': ['"Carter One"'],
+      'inter': ['Inter']
+    },
+    fontSize: {
+      'sm': '0.7rem',
+      'xl': '1.8rem',
+      '2xl': '2.5rem',
+      '3xl': '5.6rem'
+    },
+    dropShadow: {
+      'btn': '0px 4px 4px rgba(0, 0, 0, 0.50)',
+      'login-btn': '0px 4px 10px rgba(0, 0, 0, 0.50)',
+      'text': '3px 12px 4px rgba(0, 0, 0, 0.40)'
+    },
+    boxShadow: {
+      'btn': '0px 4px 4px 0px rgba(0, 0, 0, 0.50)',
+      'login-btn': '2px 4px 7px 1px rgba(0, 0, 0, 0.50)'
     },
     extend: {},
   },
