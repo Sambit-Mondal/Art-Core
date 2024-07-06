39ec767140463fca9baf96fb00aec850edea63be
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-07 01:53:35 +0530

    Update PrivateRoute.jsx

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -1,6 +1,4 @@
-// PrivateRoute.jsx
 import React, { useContext } from 'react';
-// import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import LoginPopup from '../Components/LoginPopup';
 
