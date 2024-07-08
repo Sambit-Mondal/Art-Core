27b551375e7dd10bdcc549af7f49add60f9e8a46
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-09 03:03:17 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,27 +11,30 @@ import Navbar from './Components/Navbar';
 import LoginPopup from './Components/LoginPopup';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './utils/PrivateRoute';
-import PaintingPage from './Components/PaintingPage';
+import PaintingDetails from './Screens/PaintingDetails';
 import AddArtworks from './Screens/AddArtworks';
+import { ArtworksProvider } from './context/ArtworksContext'; // Import the provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <div className='select-none flex flex-col justify-center items-center z-40 bg-opacity-10 h-screen bg-black'>
+      <ArtworksProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<><Navbar /><Home /></>} />
-            <Route path="/about" element={<><Navbar /><About /></>} />
-            <Route path="/artworks" element={<PrivateRoute element={<><Navbar /><Artworks /></>} />} />
-            <Route path="/contact" element={<PrivateRoute element={<><Navbar /><ContactMe /></>} />} />
-            <Route path="/login" element={<><Navbar /><LoginPopup loginVisibility={() => { }} /></>} />
-            <Route path="/signup" element={<><Navbar /><LoginPopup loginVisibility={() => { }} /></>} />
-            <Route path='/add-artworks' element={<PrivateRoute element={<><Navbar /><AddArtworks /></>} />} />
-            <Route path="/painting-page" element={<><Navbar /><PaintingPage /></>} />
-          </Routes>
+          <div className='select-none flex flex-col justify-center items-center z-40 bg-opacity-10 h-screen bg-black'>
+            <Routes>
+              <Route path="/" element={<><Navbar /><Home /></>} />
+              <Route path="/about" element={<><Navbar /><About /></>} />
+              <Route path="/artworks" element={<PrivateRoute element={<><Navbar /><Artworks /></>} />} />
+              <Route path="/contact" element={<PrivateRoute element={<><Navbar /><ContactMe /></>} />} />
+              <Route path="/login" element={<><Navbar /><LoginPopup loginVisibility={() => { }} /></>} />
+              <Route path="/signup" element={<><Navbar /><LoginPopup loginVisibility={() => { }} /></>} />
+              <Route path='/add-artworks' element={<PrivateRoute element={<><Navbar /><AddArtworks /></>} />} />
+              <Route path="/painting-details/:id" element={<><Navbar /><PaintingDetails /></>} />
+            </Routes>
+          </div>
         </Router>
-      </div>
+      </ArtworksProvider>
     </AuthProvider>
   </React.StrictMode>
 );
