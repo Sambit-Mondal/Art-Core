a2ebe1fe2954a55c9bb58ebef9469a5a92ec9b76
Sambit Mondal <sambitmondal2005@gmail.com>
2024-07-16 17:17:11 +0530

    Update backend

--- client/src/Screens/AddArtworks.jsx
+++ client/src/Screens/AddArtworks.jsx
@@ -46,7 +46,7 @@ function AddArtworks() {
         };
 
         try {
-            await axios.post('http://localhost:5000/api/artworks', artwork);
+            await axios.post('https://art-core-backend.onrender.com/api/artworks', artwork);
             toast.success('Artwork uploaded successfully!', {
                 position: 'top-center'
             });
